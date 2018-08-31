e18b3500e7f24359b06620ee5dba44dc8e815d58
lei2j <986111634@qq.com>
2018-09-01 00:04:22 +0800

    commit

--- src/components/roomList.vue
+++ src/components/roomList.vue
@@ -1,10 +1,10 @@
 <template>
-	<div>
+	<div id="container">
 	<el-row>
-	  <el-col :span="8" v-for="(item,index) in roomList" :key="item.room_id">
-	    <el-card :body-style="{ padding: '0px' }">
-	      <img class="image" v-bind:src="item.room_src"/>
-	      <div style="padding: 14px;">
+	  <el-col class="room-col" :span="8" v-for="(item,index) in roomList" :key="item.room_id">
+	    <el-card class="room-mod-link" :body-style="{ padding: '0px' }">
+	      <img class="room-image" v-bind:src="item.room_src"/>
+	      <div class="room-text">
 	        <span>{{item.nickname}}</span>
 	        <div class="bottom clearfix">
 	          <span>{{item.room_name}}</span>
@@ -43,5 +43,62 @@
 	}
 </script>
 	
-<style>
+<style scoped="scoped">
+	#container{
+		position: relative;
+		overflow: hidden;
+		padding: 0 4px;
+		margin: 0 auto;
+	}
+	@media screen and (max-width: 1335px){
+		.room-col .room-mod-link{
+			width: 356px;
+			height: 280px;
+		}
+		.room-col{
+			width: 366px;
+			vertical-align: top;
+		    display: inline-block;
+		    padding:0 5px;
+		    margin-bottom: 18px;
+		}
+	}
+	@media screen and (min-width: 1336px) and (max-width:1765px){
+		.room-col .room-mod-link{
+			width: 422px;
+			height: 280px;
+		}
+		.room-col{
+			width: 434px;
+			vertical-align: top;
+		    display: inline-block;
+		    padding:0 5px;
+		    margin-bottom: 18px;
+		}
+	}
+	@media only screen and (min-width:1766px) {
+  		.room-col .room-mod-link{
+			width: 528px;
+			height: 380px;
+		}
+		.room-col{
+			width: 536px;
+			vertical-align: top;
+		    display: inline-block;
+		    padding:0 5px;
+		    margin-bottom: 18px;
+		}
+  	}
+	
+	.room-mod-link .room-image{
+		display: block;
+	    width: 100%;
+	    height: 100%;
+	    transition: all .25s ease-in-out;
+	    -webkit-backface-visibility: hidden;
+	    backface-visibility: hidden;
+	}
+	.room-col .room-text{
+		/*min-width: 356px;*/
+	}
 </style>
