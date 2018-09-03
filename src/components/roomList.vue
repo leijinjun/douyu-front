a26d3270b9c89092ca5f3ac5ced70a12f56fb2e5
lei2j <986111634@qq.com>
2018-09-04 00:33:52 +0800

    commit

--- src/components/roomList.vue
+++ src/components/roomList.vue
@@ -1,19 +1,43 @@
 <template>
 	<div id="container">
-	<el-row>
-	  <el-col class="room-col" :span="8" v-for="(item,index) in roomList" :key="item.room_id">
-	    <el-card class="room-mod-link" :body-style="{ padding: '0px' }">
-	      <img class="room-image" v-bind:src="item.room_src"/>
-	      <div class="room-text">
-	        <span>{{item.nickname}}</span>
-	        <div class="bottom clearfix">
-	          <span>{{item.room_name}}</span>
-	          <el-button type="text" class="button">login</el-button>
-	        </div>
-	      </div>
-	    </el-card>
-	  </el-col>
-	</el-row>
+		<el-row>
+		  <el-col class="room-col" :span="8" v-for="(item,index) in roomList" :key="item.room_id">
+		    <el-card class="room-mod-link" :body-style="{ padding: '0px' }">
+		      <img class="room-image" v-bind:src="item.room_src"/>
+		      <div class="room-text">
+		        <label>主播：</label><span>{{item.nickname}}</span>
+		        <div class="bottom clearfix">
+		          <label>房间名：</label><span>{{item.room_name}}</span>
+		        </div>
+		        <div>
+		        	<label>人气：</label><span>{{item.hn}}</span><br />
+		        	<el-button class="btn">login</el-button>
+		        </div>
+		      </div>
+		    </el-card>
+		  </el-col>
+		</el-row>
+		<!--<div class="room-list m-col" v-for="(item,index) in roomList" :key="item.room_id">
+			<div class="room-img">
+				<img v-bind:src="item.room_src"/>
+			</div>
+			<div class="room-t">
+				<div>
+				<label class="label">房间ID：</label><span class="room-text">{{item.room_id}}</span></div>
+				<div>
+					<label class="label">房间标题：</label><span class="room-text">{{item.room_name}}</span>
+				</div>
+				<div>
+					<label class="label">房间人气：</label><span class="room-text">{{item.online}}</span>
+				</div>
+				<div>
+					<label class="label">房间粉丝：</label><span class="room-text">{{item.hn}}</span>
+				</div>
+				<div>
+					<label class="label">主播：</label><span class="room-text">{{item.nickname}}</span>
+				</div>
+			</div>
+		</div>-->
 	</div>
 </template>
 
@@ -61,6 +85,14 @@
 		    display: inline-block;
 		    padding:0 5px;
 		    margin-bottom: 18px;
+		    border-radius: 4px;
+		}
+		.room-list{
+			width: 1013px;
+			height: 264px;
+			border: 1px solid #DCDFE6;
+			border-radius: 4px;
+			padding:0 45px;
 		}
 	}
 	@media screen and (min-width: 1336px) and (max-width:1765px){
@@ -74,31 +106,59 @@
 		    display: inline-block;
 		    padding:0 5px;
 		    margin-bottom: 18px;
+		    border-radius: 4px;
+		}
+		.room-list{
+			width: 1201px;
+			height: 264px;
+			border: 1px solid #DCDFE6;
+			border-radius: 4px;
+			padding:0 45px;
 		}
 	}
 	@media only screen and (min-width:1766px) {
   		.room-col .room-mod-link{
 			width: 528px;
-			height: 360px;
 		}
 		.room-col{
 			width: 536px;
+			height: 401px;
 			vertical-align: top;
 		    display: inline-block;
 		    padding:0 5px;
 		    margin-bottom: 18px;
+		    border-radius: 4px;
+		}
+		.room-list{
+			width: 1475px;
+			height: 264px;
+			border: 1px solid #DCDFE6;
+			border-radius: 4px;
+			padding:0 45px;
 		}
   	}
-	
+  	.room-list.m-col{
+  		margin-bottom: 10px;
+  	}
+  	.room-mod-link{
+  		border: 1px solid #4d1e94;
+  	}
 	.room-mod-link .room-image{
 		display: block;
 	    width: 100%;
-	    height: 100%;
+	    height: 297px;
 	    transition: all .25s ease-in-out;
 	    -webkit-backface-visibility: hidden;
 	    backface-visibility: hidden;
 	}
 	.room-col .room-text{
-		/*min-width: 356px;*/
+		background-color: #f56c6c;
+		color: #DCDFE6;
+	}
+	.btn{
+		font-family: .AppleSystemUIFont;
+		font-size: 14px;
+		color: #f56c6c;
+		letter-spacing: 0;
 	}
 </style>
