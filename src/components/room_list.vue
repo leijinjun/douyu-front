1fbe260015fb5fb45369a25ccc1941cdc0c78006
lei2j <986111634@qq.com>
2018-09-23 22:58:12 +0800

    commit

--- src/components/room_list.vue
+++ src/components/room_list.vue
@@ -1,8 +1,5 @@
 <template>
 	<div id="main">
-		<div id='my-player' class='abp' style="height: 150px;">
-		    <div id='my-comment-stage' class='container'>aaa</div>
-		</div>
 		<div style="float: right;margin-right: 16px;">
 			<el-select v-model="selectedCate" clearable filterable placeholder="请选择分类" @change="cateChange()">
 			    <el-option
@@ -12,6 +9,8 @@
 			      :value="item.cateId">
 			    </el-option>
 			  </el-select>
+			  <el-input v-model="keyword" placeholder="请输入房间ID" style="width: 120px;"></el-input>
+			  <el-button plain @click="searchRoom()">搜索房间</el-button>
 			<el-button style="background-image: linear-gradient(-90deg, #BB9BF1 0%, #887BF2 100%);
 border-radius: 4px;color: #F7F0F0;" round @click="changeRoomList()">
 				<span v-if="!changeFlag">切换所有房间</span>
@@ -19,7 +18,7 @@ border-radius: 4px;color: #F7F0F0;" round @click="changeRoomList()">
 			</el-button>
 		</div>
 		<el-row style="margin-top: 54px;">
-		  <el-col class="room-col" :span="8" v-for="(item,index) in roomList" :key="item.roomId">
+		  <el-col v-if="roomList.length>0" class="room-col" :span="8" v-for="(item,index) in roomList" :key="item.roomId">
 		    <el-card class="room-mod-link" :body-style="{ padding: '0px' }">
 		      <router-link :to="'/room/'+item.roomId">
 			      <img class="room-image" v-bind:src="item.roomSrc"/>
@@ -37,6 +36,9 @@ border-radius: 4px;color: #F7F0F0;" round @click="changeRoomList()">
 		      </div>
 		    </el-card>
 		  </el-col>
+		 <div style="margin-top: 50px;">
+		 	暂无数据
+		 </div>
 		</el-row>
 	</div>
 </template>
@@ -49,12 +51,20 @@ border-radius: 4px;color: #F7F0F0;" round @click="changeRoomList()">
 				changeFlag:true,
 				roomId:null,
 				cates:[],
+				keyword:null,
 				selectedCate:null,
 				roomList:[]
 			}
 		},
 		created(){
-			this.getRoomList();
+			var connected=this.$route.query.connected;
+			if(connected===undefined){
+				this.changeFlag=false;
+				this.getRoomList();
+			}else{
+				this.changeFlag=true;
+				this.getCacheRoomList();
+			}
 			this.getCates();
 		},
 		methods:{
@@ -77,9 +87,7 @@ border-radius: 4px;color: #F7F0F0;" round @click="changeRoomList()">
 				this.$http.post(`/auth/login/${room}`)
 					.then((response)=>{
 						var res=response.data;
-						if(res.code==200){
-							$this.getRoomList();
-						}
+						$this.changeRoomList();
 					});
 			},
 			disConnect(room){
@@ -89,9 +97,7 @@ border-radius: 4px;color: #F7F0F0;" round @click="changeRoomList()">
 				this.$http.post('/auth/logout',params)
 					.then((response)=>{
 						var res=response.data;
-						if(res.code==200){
-							$this.getRoomList();
-						}
+						$this.changeRoomList();
 					})
 			},
 			getCacheRoomList(){
@@ -99,10 +105,8 @@ border-radius: 4px;color: #F7F0F0;" round @click="changeRoomList()">
 				this.$http.get('/room/logged')
 					.then((response)=>{
 						var res=response.data;
-						if(res.code==200){
-							$this.roomList=res.body;
-							$this.changeFlag=false;
-						}
+						$this.roomList=res.body;
+						$this.changeFlag=false;
 					})
 			},
 			changeRoomList(){
@@ -127,6 +131,20 @@ border-radius: 4px;color: #F7F0F0;" round @click="changeRoomList()">
 							$this.cates=res.body;
 						}
 					})
+			},
+			searchRoom(){
+				var room=this.keyword;
+				if(room==null||room==''){
+					return;
+				}
+				var $this=this;
+				this.$http.get(`/room/detail/${room}`)
+					.then((response=>{
+						var res=response.data;
+						var arr=new Array();
+						arr.push(res.body.room);
+						$this.roomList=arr;
+					}));
 			}
 		},
 		mounted () {
