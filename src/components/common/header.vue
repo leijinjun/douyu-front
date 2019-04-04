a4e9a6b971322bf3e382cb7f21581ce8343273d8
leijinjun <leijinjun@panpay.com.cn>
2019-04-04 09:44:50 +0800

    commit

--- src/components/common/header.vue
+++ src/components/common/header.vue
@@ -1,7 +1,8 @@
 <template>
     <div id="header">
-        <el-menu style="height: 50px;" class="header-menu" :router="true" mode="horizontal" background-color="#fff" text-color="#000" active-text-color="#5a2dff">
+        <el-menu style="height: 65px;" class="header-menu" :router="true" mode="horizontal" background-color="#fff" text-color="#000" active-text-color="#5a2dff">
         <el-menu-item  index="1" route="/">首页</el-menu-item>
+        <el-menu-item  index="2" route="/danmu">弹幕查询</el-menu-item>
     </el-menu>
     </div>
 </template>
@@ -27,12 +28,15 @@ export default {
 </script>
 <style scoped>
 #header .header-menu{
-	height: 49px;
+	height: 58px;
 }
 #header .header-menu>li{
 	height: 100%;
-	line-height: 50px;
-    font-size: 18px;
+	line-height: 58px;
+    font-size: 20px;
+}
+#header .header-menu>li:hover{
+    background-color: #fff!important;
 }
 #header .el-menu--horizontal{
     border-bottom: 0;
