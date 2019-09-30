c4f49a4d4c7c32122a118311fa6f0b9ade5f8350
leijinjun <leijinjun@panpay.com.cn>
2019-09-30 11:27:44 +0800

    commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,15 +1,33 @@
 import $http from '@/utils/request'
 export default {
     getTodayGiftRankingList(params){
-        return $http.get('/gift/view/today/rankingList',{params:params});
+        return $http.get('/gift/view/today/rankingList',{params});
     },
     getTodayDanmuRankingList(params){
-        return $http.get('/danmu/view/today/rankingList',{params:params});
+        return $http.get('/danmu/view/today/rankingList',{params});
     },
     getDanmuPage(params){
-        return $http.get('/danmu',{params:params});
+        return $http.get('/danmu',{params});
     },
     getTodayUserRankingList(params){
-        return $http.get('/gift/view/today/userRankingList',{params:params});
+        return $http.get('/gift/view/today/userRankingList',{params});
+    },
+    getFansNum(roomId,params){
+        return $http.get(`/room/info/view/fansPersonNum/${roomId}`,{params});
+    },
+    getRoomInfo(roomId,params){
+        return $http.get(`/room/info/${roomId}`,{params:params});
+    },
+    getGiftMoney(roomId,params){
+        return $http.get(`/room/info/view/giftMoney/${roomId}`,{params});
+    },
+    getGiftNum(roomId,params){
+        return $http.get(`/room/info/view/giftPersonNum/${roomId}`,{params});
+    },
+    getChatNum(roomId,params){
+        return $http.get(`/room/info/view/chatSum/${roomId}`,{params});
+    },
+    getChatSum(roomId,params){
+        return $http.get(`/room/info/view/chatPersonNum/${roomId}`,{params});
     }
 }
